d284c8963c31c6fd3403e0769c606881049d3f2e
JOSHUA5227 <joshua.quinthino@gmail.com>
2026-05-28 04:25:27 +0000

    marked todos

--- src/design/apb_master.v
+++ src/design/apb_master.v
@@ -1,12 +1,12 @@
 module apb_master(
-input PCLK,PRESETn,transfer,READ_WRITE,
-input apb_write_paddr,apb_read_paddr,
-input apb_write_data,
-input PREADY,
-input PRDATA,
-output PWRITE,PSEL1,PENABLE,
-output paddr,pwdata,apb_read_data_out,
-output mux_sel
+input PCLK,PRESETn,transfer,READ_WRITE, //done
+input apb_write_paddr,apb_read_paddr, //done
+input apb_write_data, //done
+input PREADY, //done
+input PRDATA, // todo
+output PWRITE,PSEL1,PENABLE, //done
+output paddr,pwdata,apb_read_data_out, // todo
+output mux_sel // done but might have to change
 );
 
 localparam [1:0] idle =2'b00,setup=2'b01,access =2'b10;
@@ -15,6 +15,8 @@ reg [1:0] ps,ns;
 reg next_sel,next_en,next_pwrite;
 reg next_paddr_R,next_padd_W;
 reg next_mux_sel;
+reg next_write_data;
+
 
 always@(posedge PCLK or PRESETn)
 begin
@@ -41,14 +43,17 @@ begin
 	next_en = 0;
 	next_pwrite = 0;
 	next_mux_sel = 0;
+	next_write_data = 0;
+
 
 	case(ps)
 	idle:
 	begin
 		next_pwrite = (READ_WRITE);
 		next_mux_sel = (READ_WRITE) ? apb_read_paddr[8]:apb_write_paddr[8];
-		next_paddr_R = 
-		next_paddr_W = 
+		next_paddr_R = apb_write_paddr;
+		next_paddr_W = apb_read_paddr;
+		next_write_data = apb_write_data;
 		next_sel = 0;
 		next_en = 0;
 
